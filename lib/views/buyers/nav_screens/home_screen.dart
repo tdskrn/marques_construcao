9e7ec0d8197b0a8935cdbdf5b4eca2ae735c0203
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-03-10 22:17:39 -0300

    category text

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marques_construcao/views/buyers/nav_screens/widgets/banner_widget.dart';
+import 'package:marques_construcao/views/buyers/nav_screens/widgets/category_text.dart';
 import 'package:marques_construcao/views/buyers/nav_screens/widgets/search_input_widget.dart';
 import 'package:marques_construcao/views/buyers/nav_screens/widgets/welcome_text_widget.dart';
 
@@ -9,13 +10,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         WelcomeText(),
         SizedBox(
           height: 14,
         ),
         SearchInputWidget(),
-        BannerWidget()
+        BannerWidget(),
+        CategoryText(),
       ],
     );
   }
